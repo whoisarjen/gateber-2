03bd931d735ed8a495e9e7bafee1354bbcc0fe35
Kamil Owczarek <80225782+whoisarjen@users.noreply.github.com>
2024-01-26 14:16:10 +0000

    hello world

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+import { env } from "@/env.mjs";
+import { SiteConfig } from "types"
+
+const site_url = env.NEXT_PUBLIC_APP_URL;
+
+export const siteConfig: SiteConfig = {
+  name: "Gateber",
+  description:
+    "Get your project off to an explosive start with Gateber! Harness the power of Next.js 14, Prisma, Planetscale, Auth.js, Resend, React Email, Shadcn/ui and Stripe to build your next big thing.",
+  url: site_url,
+  ogImage: `${site_url}/og.jpg`,
+  links: {
+    twitter: "https://twitter.com/miickasmt",
+    github: "https://github.com/mickasmt/next-saas-stripe-starter",
+  },
+  mailSupport: "support@Gateber.com"
+}
