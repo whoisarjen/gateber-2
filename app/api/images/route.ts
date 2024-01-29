0bf29aca4868a45d616126ef79b0a942b67786de
whoisarjen2 <157956920+whoisarjen2@users.noreply.github.com>
2024-01-29 12:53:13 +0000

    fix type on route

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,4 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server';
 import fs from 'fs'
 import path from 'path'
@@ -6,7 +5,7 @@ import path from 'path'
 const filePath = path.resolve('.', 'assets/images/accessories_art.webp')
 const imageBuffer = fs.readFileSync(filePath)
 
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+export const GET = async () => {
     const response = new NextResponse(imageBuffer)
     response.headers.set('content-type', 'image/png');
     return response;
